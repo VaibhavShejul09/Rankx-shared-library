06c7114903714a696e637f6ff3272c28d8e06b8f
VaibhavShejul09 <shejulv088@gmail.com>
2026-03-06 10:09:53 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -2,7 +2,7 @@ def call(String serviceName) {
     stage('Checkout Helm Charts & deploy') {
                 script {
                   withCredentials([string(credentialsId: 'git-token', variable: 'GIT_TOKEN')]) {
-                    sh '''
+                    sh """
                          # Clean previous helm repo folder
                          # rm -rf rankx-environments
 
@@ -11,14 +11,13 @@ def call(String serviceName) {
                          git clone https://VaibhavShejul09:$GIT_TOKEN@github.com/VaibhavShejul09/centralized-helm-repo.git
 
                          #kubectl create namespace my-app
-                         helm upgrade --install ${serviceName} centralized-helm-repo/charts/microservice-base \
-                         -f rankx-environments/$ENVIRONMENT/${serviceName}/values.yaml \
+                         helm upgrade --install ${serviceName} centralized-helm-repo/charts/microservice-base -f rankx-environments/$ENVIRONMENT/${serviceName}/values.yaml \
                          --namespace $ENVIRONMENT \
                          --create-namespace \
                          --atomic --wait \
                          --timeout 5m --debug
 
-                   '''
+                   """
                
              }
         }
