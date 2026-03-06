e98ce84326db481922c472366a4a364526ca57a5
VaibhavShejul09 <shejulv088@gmail.com>
2026-03-06 10:26:49 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -5,7 +5,7 @@ def call(String serviceName) {
                          # Clean previous helm repo folder
                          # rm -rf rankx-environments
 
-                         # git clone $ENV_HELM_REPO 
+                         
                          # Clone the helm / environment repo
                          git clone https://VaibhavShejul09:$GIT_TOKEN@github.com/VaibhavShejul09/centralized-helm-repo.git
 
